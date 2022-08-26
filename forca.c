2f2c306a3589a94d3c9e4018062ed121610d7e31
Rajora0 <rafaelj492@gmail.com>
2022-08-26 15:59:28 -0300

    Começando jogo de forca...

---
+++ forca.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main () {
+
+    // imprime cabeçalho
+    printf("*******************************************\n");
+    printf("*    Bem vindo ao nosso jogo de forca!    *\n");
+    printf("*******************************************\n");
+
+    
+}
